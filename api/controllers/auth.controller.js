878c5e1bbf71893aca9a3c30f2fe0069e07bd780
jackbasso <jackbasso.r@gmail.com>
2024-04-06 22:23:04 -0600

    db user create

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,25 @@
-export const register = (req, res) => {
-  console.log(req.body);
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  //HASH THE PASSWORD WITH BCRYPT
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
+
+  //CREATE A NEW USER AND SAVE TO DB
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  console.log(newUser);
 };
+
 export const login = (req, res) => {
   //deb operations
 };
