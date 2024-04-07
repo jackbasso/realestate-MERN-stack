2cb3bd43f2ad802c00f55b5f2d14e880340b7eff
jackbasso <jackbasso.r@gmail.com>
2024-04-06 20:46:46 -0600

    first routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const register = (req, res) => {
+  console.log(req.body);
+};
+export const login = (req, res) => {
+  //deb operations
+};
+
+export const logout = (req, res) => {
+  //deb operations
+};
